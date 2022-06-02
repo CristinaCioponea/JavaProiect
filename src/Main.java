846d09132c64b2bb8a0ddbd6fb4cb013bbfec490
Cioponea Cristina <jula.cristina@yahoo.com>
2022-06-02 15:17:57 +0300

    Cristina2

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,27 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println(" Hello" +
-                " My world " +
-                "" +
-                "");
-        System.out.println("test");
+                " Cristina ");
+
+        byte a = 2;
+        byte b = 6;
+        int c = a + b;
+        System.out.println(c);
+
+        int d = b / a;
+        System.out.println(d);
+
+        System.out.println(-5 + 8 * 6);
+        System.out.println( (55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
     }
 
 
 }
+
+
+
+
