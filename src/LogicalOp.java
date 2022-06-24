72cf057b4f75adfcdbc7c16d8c60e56b8769b465
Cioponea Cristina <jula.cristina@yahoo.com>
2022-06-24 10:18:24 +0300

    Cristina2

---
+++ src/LogicalOp.java
@@ -0,0 +1,33 @@
+public class LogicalOp {
+
+    public int checkBiggerNumber(int first, int second) {
+        if (first > second) {
+            return first;
+        } else {
+            return second;
+        }
+    }
+
+
+        String text = "FastTrackIt";
+        int number = 3;
+
+        public int checkNumber () {
+        if (number <= 3) {
+            System.out.println("FastTrackIt" + number);
+        } else if (number >= 4) {
+            System.out.println(number + "FastTrackIt");
+        }
+        return checkNumber();
+
+    }
+    }
+
+
+
+
+
+
+
+
+
--- src/Main3.java
+++ src/Main3.java
@@ -1,17 +1,15 @@
 public class Main3 {
     public static void main(String[] args) {
-
-
         String textNew = "FastTrackIt";
-
         if (textNew.equals("FastTrackIt")) {
             System.out.println("Learning text comparison" + textNew);
         } else if (!textNew.equals("FastTrackIt")) ;
         {
-
             System.out.println("Got to try some more" + textNew);
-
         }
+            LogicalOp op = new LogicalOp();
+            int biggest = op.checkBiggerNumber(7,3);
+            System.out.println("The bigger number is: " + biggest);
     }
 
 
