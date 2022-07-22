9f51adf78a0d25d233bf7f2677af863e5246d971
Cioponea Cristina <jula.cristina@yahoo.com>
2022-07-22 11:17:52 +0300

    Cristina2

--- src/Main7.java
+++ src/Main7.java
@@ -2,6 +2,6 @@ public class Main7 {
     public static void main(String[] args) {
         Read read = new Read();
         float s = read.getFloat();
-        System.out.println(s);
+        System.out.print(s);
     }
 }
--- src/Read.java
+++ src/Read.java
@@ -9,16 +9,21 @@ public class Read {
         Scanner scann = new Scanner(System.in);
         do {
             try {
-                System.out.println("Just a number ");
+                System.err.print(" ");
                 number = scann.nextFloat();
                 repeat = false;
             } catch (InputMismatchException exception) {
-                System.err.println("No valid! " + scann.next());
+                System.err.print("No valid! " + scann.next());
                 repeat = true;
 
             }
         } while (repeat);
         return number;
-
     }
+    //1.In clasa Read, din programul vostru, creati o metoda tratat, care sa returneze un numar de la tastatura
+    public int tratat(){
+
+        return 0;
+    }
+
 }
